531651b5707a668a7f2ce047d83e3f0bda99200c
AliuXxHyper <101894358+AliuXxHyper@users.noreply.github.com>
2022-10-24 20:31:20 -0300

    page

---
+++ Pages/Gallery/data.js
@@ -0,0 +1,11 @@
+const requestURL = 'https://my-json-server.typicode.com/AliuXxHyper/API_Museo_2022/';
+
+const request = new XMLHttpRequest();
+
+function onRequestHandler() {
+    if (this.readyState === 4 && this.status === 200){
+    
+    }
+}
+
+//Aun no esta terminado\\
