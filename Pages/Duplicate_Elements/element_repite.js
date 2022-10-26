c0d64a397bae3cf4df83a6671717a4b1854f4554
AliuXxHyper <101894358+AliuXxHyper@users.noreply.github.com>
2022-10-25 21:11:45 -0300

    Add files via upload

---
+++ Pages/Duplicate_Elements/element_repite.js
@@ -0,0 +1,42 @@
+class header_duplicate extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = 
+            `<header>
+                <div>
+                    <img src="../../Imgs/logo.png" alt="">
+                    <p>zOpen</p>
+                </div>
+                <nav>
+                    <a href="../Home/index.html">Home</a>
+                    <a href="../Gallery/gallery.html">Gallery</a>
+                    <a href="../About_us/about_us.html">About us</a>
+                    <a href="../Tickets/ticket_buy.html">Boleteria</a>
+                </nav>
+            </header>`;
+    }
+}
+
+window.customElements.define("header-duplicate", header_duplicate);
+
+class footer_duplicate extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `
+        <footer>
+        <div>
+        <p>En colaboración de Racoon pictures</p>
+        </div>
+        
+        <div>
+            <a href="facebook.com"><img src="../../Imgs/social_networks/Facebook_logo-1048x1048.png" alt=""></a>
+            <a href="twitter.com"><img src="../../Imgs/social_networks/Twitter_logo-1048x1048.png" alt=""></a>
+            <a href="instagram.com"><img src="../../Imgs/social_networks/Instagram_logo-1048x1048.png" alt=""></a>
+        </div>
+        
+        <div>
+        <p>Todos los derechos resevados Alvaro Amestoy</p>
+        </div>
+    </footer>`;
+    }
+}
+
+window.customElements.define("footer-duplicate", footer_duplicate);
